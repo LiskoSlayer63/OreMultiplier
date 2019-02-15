38f6776f51b5e8dfa4cc870ce19c754409198e28
LiskoSlayer63 <markus.ahola@hotmail.com>
2019-02-16 01:47:04 +0200

    Tweaked configuration

--- src/main/java/liznet/oredrop/BreakHandler.java
+++ src/main/java/liznet/oredrop/BreakHandler.java
@@ -3,7 +3,7 @@ package liznet.oredrop;
 import java.util.Arrays;
 import java.util.Random;
 
-import liznet.oredrop.Config.ODConfig;
+import liznet.oredrop.Config;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.init.Blocks;
@@ -18,7 +18,7 @@ public class BreakHandler {
 	public static void onPlayerHarvestEvent(HarvestDropsEvent event) {
     	// Check if there's harvester and if the mod is enabled
 		
-		if (event.getHarvester() != null && ODConfig.extraDropEnabled) {
+		if (event.getHarvester() != null && Config.extraDropEnabled) {
 			
 			if(event.isSilkTouching() || !event.getState().getMaterial().equals(Material.ROCK)){
 				// Player is using Silk Touching or the material isn't considered as a rock
@@ -34,13 +34,13 @@ public class BreakHandler {
 			}
 
 			try {
-				if ((!ODConfig.blacklistMode && Arrays.asList(ODConfig.extraDropOres).contains(block.getRegistryName().toString())) || (ODConfig.blacklistMode && !Arrays.asList(ODConfig.extraDropOres).contains(block.getRegistryName().toString()))) {
+				if ((!Config.blacklistMode && Arrays.asList(Config.extraDropOres).contains(block.getRegistryName().toString())) || (Config.blacklistMode && !Arrays.asList(Config.extraDropOres).contains(block.getRegistryName().toString()))) {
 					Random rand = new Random();
 					
 					for(ItemStack itemStack : event.getDrops()){
 						if(!itemStack.getItem().getRegistryName().equals(block.getRegistryName()) && !(itemStack.getItem() instanceof ItemBlock)){
 							// Item doesn't drop itself and the dropped item wasn't an ItemBlock
-							int multi = rand.nextInt(ODConfig.maxDropMulti - ODConfig.minDropMulti + 1) + ODConfig.minDropMulti;
+							int multi = rand.nextInt(Config.maxDropMulti - Config.minDropMulti + 1) + Config.minDropMulti;
 							itemStack.setCount(itemStack.getCount() * multi);
 						}
 					}
--- src/main/java/liznet/oredrop/Config.java
+++ src/main/java/liznet/oredrop/Config.java
@@ -2,31 +2,40 @@ package liznet.oredrop;
 
 import net.minecraftforge.common.config.Config.Comment;
 import net.minecraftforge.common.config.Config.Name;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent.OnConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+@net.minecraftforge.common.config.Config(modid = OreDrop.modId)
 public class Config {
+	@Comment({"Enables this mod"})
+	@Name("Enabled")
+	public static boolean extraDropEnabled = true;
 
-	@net.minecraftforge.common.config.Config(modid = OreDrop.modId)
-	public static class ODConfig{
-		
-		@Comment({"Enables this mod"})
-		@Name("Enabled")
-		public static boolean extraDropEnabled = true;
+	@Comment({"Minimum multiplier for drops"})
+	@Name("Min. multiplier")
+	public static int minDropMulti = 2;
+	
+	@Comment({"Maximum multiplier for drops"})
+	@Name("Max. multiplier")
+	public static int maxDropMulti = 4;
+	
+	@Comment({"List of ores that should drop extra loot"})
+	@Name("Ore list")
+	public static String[] extraDropOres = new String[]{};
 
-		@Comment({"Minimum multiplier for drops"})
-		@Name("Min. multiplier")
-		public static int minDropMulti = 2;
-		
-		@Comment({"Maximum multiplier for drops"})
-		@Name("Max. multiplier")
-		public static int maxDropMulti = 4;
-		
-		@Comment({"List of ores that should drop extra loot"})
-		@Name("Ore list")
-		public static String[] extraDropOres = new String[]{};
-
-		@Comment({"If enabled this inverts the ore list so every ore that drops items instead of the block itself will multiply, except those in the list"})
-		@Name("Blacklist mode")
-		public static boolean blacklistMode = true;
-	}
-
+	@Comment({"If enabled this inverts the ore list so every ore will multiply, except those in the list"})
+	@Name("Blacklist mode")
+	public static boolean blacklistMode = true;
+	
+	@EventBusSubscriber(modid = OreDrop.modId)
+	private static class EventHandler {
+		@SubscribeEvent
+		public static void onConfigChanged(final OnConfigChangedEvent event) {
+			if (event.getModID().equals(OreDrop.modId)) {
+				ConfigManager.sync(OreDrop.modId, net.minecraftforge.common.config.Config.Type.INSTANCE);
+			}
+		}
+}
 }
